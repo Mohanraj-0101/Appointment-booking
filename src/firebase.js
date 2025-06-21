119b82c87ee116aaba31ecdf13be87a1d32597a2
MOHANRAJ R <mohanraj.r.personal@gmail.com>
2025-06-21 11:13:32 +0530

    Hidden the firebase key

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyCVYqjpqlUcBsIjc7LzTDUFRK0l7mnRGLw",
-  authDomain: "appointment-booking-af98f.firebaseapp.com",
-  projectId: "appointment-booking-af98f",
-  storageBucket: "appointment-booking-af98f.appspot.com",
-  messagingSenderId: "203593465418",
-  appId: "1:203593465418:web:b5809c5d1eabbaef80b0b3"
-};
+//it is  in local txt file
 
 // Prevent re-initialization during hot reload
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
